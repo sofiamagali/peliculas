236dbb3f4818786a37ede20b886373c325e00523
sofi.magali <sofia.fuhrmann@gmail.com>
2021-12-22 21:42:52 -0300

    Comienzo grilla
    Comienzo grilla peliculas

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { PeliculasListado } from "./PeliculasListado";
+
+export function App() {
+    return <div> 
+        <header>Peliculas</header>
+        <main>
+          <PeliculasListado />
+        </main>
+    </div>
+}
---
+++ src/PeliculasListado.jsx
@@ -0,0 +1,11 @@
+export function PeliculasListado() {
+    return (
+    <ul>
+    <li>Peli1</li>
+    <li>Peli2</li>
+    <li>Peli3</li>
+    <li>Peli4</li>
+    <li>Peli5</li>
+</ul>
+    );
+}
